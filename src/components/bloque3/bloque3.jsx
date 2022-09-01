4d79095ac7b980dbe5bf01f118a5cc5ec514d4a3
florangel ramirez <yamilex1219@gmail.com>
2022-09-01 13:23:43 +0200

    bloque3

---
+++ src/components/bloque3/bloque3.jsx
@@ -0,0 +1,26 @@
+
+import './estilob3.css';
+
+
+function bloque3() {
+  return (
+    <section className="bloque">
+        <h1> </h1>
+
+        <div>
+
+            <div className='flormulario'></div>
+            <div className='mapa'>
+                <div className='apiMapa'></div>
+                <div className='apiTiempo'></div>
+            </div>
+            
+        </div>
+            
+    </section>
+  );
+}
+
+
+
+export default bloque3;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Portada from './components/portada';
+import bloque3 from './components/bloque3/bloque3';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Portada />
+    <bloque3 />
   </React.StrictMode>
 );
 
