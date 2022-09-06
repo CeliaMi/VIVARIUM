72bd6e8a63f3f2724cc77b21b2294fd1f8282674
Celia <celiamillanalemany@gmail.com>
2022-09-06 12:55:08 +0200

    estructuragrupo3+grupo1

--- src/index.js
+++ src/index.js
@@ -2,22 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import Portada from './components/portada';
-<<<<<<< HEAD
 import Bloque3 from './components/bloque3/bloque3';
-=======
 import Footer from './components/footer';
->>>>>>> 0f19d63dceb7ab6ee0af8611c37aa528df24ce8f
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Portada />
-<<<<<<< HEAD
     <Bloque3 />
-=======
     <Footer />
->>>>>>> 0f19d63dceb7ab6ee0af8611c37aa528df24ce8f
   </React.StrictMode>
 
 );
