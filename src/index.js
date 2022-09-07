92af3acc615677db3c19a3f89bafe010029abfab
emilymsq <emilymosq4@gmail.com>
2022-09-07 11:08:52 +0200

    cambios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './App.css';
 import Portada from './components/portada';
 import Footer from './components/footer';
@@ -8,23 +7,12 @@ import Bloque3 from './components/bloque3/bloque3';
 import Catalogo from './components/catalogo';
 import './index.css';
 import './App';
-=======
-import './index.css';
-import App from './App';
->>>>>>> 7cbdab68c04bd25c10d5dadd31ca995cbb997584
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <Portada />
-    <Catalogo />
-    <Bloque3 />
-    <Footer />
-=======
     <App />
->>>>>>> 7cbdab68c04bd25c10d5dadd31ca995cbb997584
   </React.StrictMode>
 
 );
