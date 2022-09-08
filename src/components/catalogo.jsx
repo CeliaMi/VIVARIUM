931a5a97ac440592df1929f7a2bf8769b2202a1d
emilymsq <emilymosq4@gmail.com>
2022-09-08 10:46:29 +0200

    catalogoCambios

--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -1,18 +1,22 @@
 import React from "react"
-import especie1 from '../imagenes/pradera-de-posidonia 2.svg';
 import mas from '../imagenes/mas.svg'
 import '../css/catalogo.css';
+import Cards from '../db.json'
 
 function Catalogo() {
+
   return (
     <div className="Catalogo">
+    {
+      Cards.map(card => {
+      return(
       <div className="Cards">
-          <div className="CardsInner">
+          <div className="CardsInner"  key={ card.id }>
           {/* Tarjeta Front */}
               <div className="CardsFace CardsFaceFront">
                   <div className="CardsHeader">
-                  <img src={especie1}  alt="" className="pp" />
-							    <h2>Posidonia Oceánica</h2>
+                  <img src={ card.img }  alt="" className="ImagenEspecie" />
+							    <h2>{ card.nombre }</h2>
 							    <div className="BotonMas">
 								    <img src={mas} className="VerMas" alt="" />
 							    </div>
@@ -21,10 +25,7 @@ function Catalogo() {
          {/* Tarjeta back */}
               <div className="CardsFace CardsFaceBack">
                    <div className="CardsBody">
-						            <p>Se trata de una planta marina exclusiva del Mediterráneo, pues da refugio a gran número de
-							            especies que se alimentan, se reproducen y se cobijan en estas praderas. Además, la
-							            posidonia filtra y oxigena las aguas, por lo que su presencia nos indica la buena calidad de
-							            las mismas.</p>
+						            <p>{ card.texto }</p>
 					        </div>
 					        <div className="BotonBack">
 						      <a href="#" className="Btn">Apadrinar ❤️</a>
@@ -32,6 +33,9 @@ function Catalogo() {
               </div>
         </div>
     </div>
+    )
+  })
+  }
   </div>
   );
 }
