6e0ba8b9179bb81d59389a0cf62a0b39f4231704
Oli <oliris.of@gmail.com>
2022-09-12 12:39:15 +0200

    formularioLugar

--- src/components/bloque3/bloque3.jsx
+++ src/components/bloque3/bloque3.jsx
@@ -1,5 +1,7 @@
 
 import './estilob3.css';
+import React from 'react';
+import Formulario from './formulario';
 
 function Bloque3() {
   return (
@@ -15,66 +17,13 @@ function Bloque3() {
             <div>
               <h2 className="h2Bloque3">Apadrina</h2>
               <div className='cardFormulario'>
-                <form className='formulario'>
-                  <input className='inputform' placeholder='Nombre y apellidos' id='nomape'></input>
-                  <div className='alertita' id='alerta1'></div>
-                  <input className='inputform' placeholder='Email' id='email'></input>
-                  <div className='alertita' id='alerta2'></div>
-                  <input className='inputform' placeholder='Especies a adoptar' id='especie'></input>
-                  <div className='alertita' id='alerta3'></div>
-                  <input type='number' className='inputform' placeholder='Cantidad' id='cantidad'></input>
-                  <div className='alertita' id='alerta4'></div>
-                  <button type='button' className='enviar' value='Enviar' onClick={clickeado}>Enviar ❤</button>
-                  <div className='alertitaGuay' id='alerta5'></div>
-
-                </form>
+                <Formulario/>
               </div>
             </div>                 
     </section>
   );
 }
-function clickeado() {
-  var nombre = document.getElementById("nomape").value;
-  var correo = document.getElementById("email").value;
-  var regex = /[a-z.]+\@+[a-z]+\.+[a-z]+/g;
-  var especie = document.getElementById("especie").value;
-  var cantidad = document.getElementById("cantidad").value;
 
-  
-  var alerta1 = document.getElementById("alerta1");
-  var alerta2 = document.getElementById("alerta2");
-  var alerta3 = document.getElementById("alerta3");
-  var alerta4 = document.getElementById("alerta4");
-  var alerta5 = document.getElementById("alerta5");
-    
-  
-  if (nombre == "") {
-    alerta1.innerHTML = "Complete campo nombre"; 
-} else { 
-    alerta1.innerHTML = " "
-}
-  if (correo == "") {
-    alerta2.innerHTML = "Complete campo correo";
-} else if (!regex.test(correo)) {
-    alerta2.innerHTML= "Debe introducir un correo valido";
-} else {
-    alerta2.innerHTML = " "
-}
-  if (especie == "") {
-    alerta3.innerHTML = "Complete campo especie";
-} else {
-    alerta3.innerHTML = " "
-}
-  if (cantidad == "") {
-    alerta4.innerHTML = "Complete campo cantidad";
-} else {
-    alerta4.innerHTML = " "
-}
-  if ( nombre, correo, especie, cantidad)  {
-      alerta5.innerHTML = "Enviado 💙"
-}
-    
-}
 
 
 export default Bloque3;
