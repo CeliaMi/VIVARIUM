4ff1a878a99ed89c5033e4ed37b475f023372ea7
Oli <oliris.of@gmail.com>
2022-09-12 14:20:56 +0200

    formOtraVez2

--- src/components/bloque3/formulario.jsx
+++ src/components/bloque3/formulario.jsx
@@ -11,37 +11,31 @@ function Formulario() {
     
         
         var alerta1 = document.getElementById("alerta1");
+        var alerta6 = document.getElementById("alerta6");
         var alerta2 = document.getElementById("alerta2");
         var alerta3 = document.getElementById("alerta3");
         var alerta4 = document.getElementById("alerta4");
         var alerta5 = document.getElementById("alerta5");
         
         
-        if (nombre === "") {
-        alerta1.innerHTML = "Complete campo nombre"; 
-    } else { 
-        alerta1.innerHTML = " ";
-    }
-        if (correo === "") {
-        alerta2.innerHTML = "Complete campo correo";
-    } else if (!regex.test(correo)) {
-        alerta2.innerHTML= "Debe introducir un correo valido";
-    } else {
-        alerta2.innerHTML = " ";
-    }
-        if (especie === "") {
-        alerta3.innerHTML = "Complete campo especie";
-    } else {
-        alerta3.innerHTML = " ";
-    }
-        if (cantidad === "") {
-        alerta4.innerHTML = "Complete campo cantidad";
-    } else {
-        alerta4.innerHTML = " ";
-    }
-        if ( (nombre, correo, especie, cantidad) )  {
-            alerta5.innerHTML = "Enviado 💙";
-    }
+        if (nombre == "") {
+            alerta1.innerHTML = "Complete campo nombre"; 
+        } 
+          else if (correo == "") {
+            alerta6.innerHTML = "Complete campo correo";
+        }
+          else if (!regex.test(correo)) {
+            alerta2.innerHTML = "Debe introducir un correo valido";
+        }
+          else if (especie == "") {
+            alerta3.innerHTML = "Complete campo especie";
+        }
+          else if (cantidad == "") {
+            alerta4.innerHTML = "Indique la cantidad deseada";
+        }
+          else {
+              alerta5.innerHTML = "Enviado 💙"
+          }
         
     }
     return( 
@@ -50,6 +44,7 @@ function Formulario() {
         <div className='alertita' id='alerta1'></div>
         <input className='inputform' placeholder='Email' id='email'></input>
         <div className='alertita' id='alerta2'></div>
+        <div className='alertita' id='alerta6'></div>
         <input className='inputform' placeholder='Especies a adoptar' id='especie'></input>
         <div className='alertita' id='alerta3'></div>
         <input type='number' className='inputform' placeholder='Cantidad' id='cantidad'></input>
